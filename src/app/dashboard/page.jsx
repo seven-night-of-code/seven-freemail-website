56b2a8a40414d064b63e256306e6e1af66fef9d5
Blessed-Nur <blessednur67@gamil.com>
2024-05-26 00:43:57 +0100

    starter

---
+++ src/app/dashboard/page.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import style from "./page.module.css";
+import Link from "next/link";
+function page() {
+  return (
+    <main>
+      <nav className={style.nav}>
+        <div className="leftNav">
+          <div className="logo"></div>
+        </div>
+        <div className="rightNav">
+          <ul>
+            <li>
+              <Link></Link>
+            </li>
+            <li>
+              <Link></Link>
+            </li>
+            <li>
+              <Link></Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </main>
+  );
+}
+
+export default page;
